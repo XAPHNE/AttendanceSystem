5edc0d3c1fd1f9bb7e0feeefee201e8238bfdc7b
Prithi Rabha <127822494+prithirabha@users.noreply.github.com>
2023-08-18 14:38:58 +0530

    Update DashboardController.java
    Testing out the dashboard window

--- src/main/java/com/bsmi/attendancesystem/DashboardController.java
+++ src/main/java/com/bsmi/attendancesystem/DashboardController.java
@@ -1,4 +1,23 @@
 package com.bsmi.attendancesystem;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class DashboardController {
+    public void display(){
+        Stage window = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(DashboardController.class.getResource("fxml/dashboard-view.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+            window.initStyle(StageStyle.UNDECORATED);
+            window.setScene(scene);
+            window.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
