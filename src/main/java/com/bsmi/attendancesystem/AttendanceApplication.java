3ad5fb807aff129d0b48ecfe2888a82242c625aa
prithirabha <prithirabha@protn.me>
2023-08-20 18:10:51 +0530

    Hiding application login window when dashboard opens

--- src/main/java/com/bsmi/attendancesystem/AttendanceApplication.java
+++ src/main/java/com/bsmi/attendancesystem/AttendanceApplication.java
@@ -9,14 +9,21 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class AttendanceApplication extends Application {
+
+    private static Stage window;
     @Override
     public void start(Stage stage) throws IOException {
+        window = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(AttendanceApplication.class.getResource("fxml/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 938, 583);
         stage.initStyle(StageStyle.UNDECORATED);
         //stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static Stage getWindow(){
+        return window;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/bsmi/attendancesystem/DashboardController.java
+++ src/main/java/com/bsmi/attendancesystem/DashboardController.java
@@ -1,16 +1,34 @@
 package com.bsmi.attendancesystem;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class DashboardController {
+
+    private Stage window;
+    @FXML
+    private MFXButton logoutMFXButton;
+
+    @FXML
+    public void logoutMFXButtonOnAction(ActionEvent event){
+        window = (Stage) logoutMFXButton.getScene().getWindow();
+        window.close();
+    }
+
     public void display(){
-        Stage window = new Stage();
-        FXMLLoader fxmlLoader = new FXMLLoader(DashboardController.class.getResource("fxml/dashboard-view.fxml"));
+        Stage stage = AttendanceApplication.getWindow();
+        stage.hide();
+        window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/dashboard-view.fxml"));
         try {
             Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
             window.initStyle(StageStyle.UNDECORATED);
